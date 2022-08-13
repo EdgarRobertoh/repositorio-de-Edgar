b92508d16121709cce150148e4e750d42d030191
EdgarRobertoh <equinonezh1@miumg.edu.gt>
2022-08-13 09:54:13 -0600

    se agrega la parte dos del examen

---
+++ Examen parcial/parte dos/numeros.py
@@ -0,0 +1,11 @@
+numeros = []
+for i in range(2):
+  numero = float(input("Introduce el número #{}: ".format(i + 1)))
+  numeros.append(numero)
+
+mayor = numeros[0]
+
+for numero in numeros:
+    if numero > mayor:
+        mayor = numero
+print("Mayor:", mayor)
